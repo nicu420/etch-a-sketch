0485ef3f047068f8407e28bfef3c8357a158c86a
nicu <ontanicolae15@gmail.com>
2022-08-23 11:49:22 +0300

    Set up the "hover" effect

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ const container = document.querySelector('#container');
 for (let i = 0; i < 16; i++) {
     const div = document.createElement('div');
     div.classList.add('square');
+    div.addEventListener("mouseover", () => color(div));
     divs.push(div);
     container.appendChild(div);
 }
 
+function color(div) {
+    div.classList.add('colored');
+}
