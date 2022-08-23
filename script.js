e5a20f53e1aae34ed9456c4d582215f55454b1c4
nicu <ontanicolae15@gmail.com>
2022-08-23 12:22:39 +0300

    Implemented asking for number of squares

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
-const divs = [];
+let divs = [];
 const container = document.querySelector('#container');
 
-for (let i = 0; i < 16; i++) {
-    const div = document.createElement('div');
-    div.classList.add('square');
-    div.addEventListener("mouseover", () => color(div));
-    divs.push(div);
-    container.appendChild(div);
+const button = document.querySelector('#ask');
+
+button.addEventListener("click", generate);
+
+function generate() {
+    // clear dom element
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+
+    // empty array
+    divs = [];
+
+    // ask for number and generate
+    let numberOfSquares = parseInt(prompt('Enter a number of squares (0-100)'));
+    for (let i = 0; i < numberOfSquares; i++) {
+        const div = document.createElement('div');
+        div.classList.add('square');
+        div.addEventListener("mouseover", () => color(div));
+        divs.push(div);
+        container.appendChild(div);
+    }
+
+    // make grid
+    container.style.display = "grid";
+    container.style.gridTemplateColumns = `repeat(${Math.sqrt(numberOfSquares)}, 50px)`;
+    container.style.gridTemplateRows = `repeat(${Math.sqrt(numberOfSquares)}, 50px)`;
 }
 
 function color(div) {
